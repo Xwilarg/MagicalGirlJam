c5af265123b6db0da54fd7ea3166ac9cc1e6e847
Xwilarg <xwilarg@protonmail.com>
2022-08-19 15:57:15 +0200

    Improve aerial controls

--- Assets/Scripts/Character/ACharacter.cs
+++ Assets/Scripts/Character/ACharacter.cs
@@ -19,6 +19,9 @@ namespace MagicalGirlJam.Character
         private int _dodgeLayer;
         private int _baseLayer;
 
+        /// <summary>
+        /// Get the forward direction of the player (-1 if left or 1 if right)
+        /// </summary>
         private float ForwardDirection
             => (XMov < 0f || XMov == 0f && _rb.velocity.x < 0f) ? -1f : 1f;
 
@@ -32,6 +35,9 @@ namespace MagicalGirlJam.Character
             Init();
         }
 
+        /// <summary>
+        /// Method called by children class to init themselves
+        /// </summary>
         protected virtual void Init()
         { }
 
@@ -41,40 +47,24 @@ namespace MagicalGirlJam.Character
             {
                 return;
             }
-            _rb.velocity = new Vector2(XMov * Time.deltaTime * _info.Speed, _rb.velocity.y);
-        }
-
-        protected void Dash()
-        {
-            if (_canMove && _canUseDash)
+            if (IsOnFloor)
             {
-                _rb.AddForce(Vector2.right * _info.DashForce * ForwardDirection, ForceMode2D.Impulse);
-                StartCoroutine(WaitAndReloadDash());
+                _rb.velocity = new Vector2(XMov * Time.deltaTime * _info.Speed, _rb.velocity.y);
             }
-        }
-
-        protected void Jump()
-        {
-            if (_canMove)
+            else
             {
-                var hit = Physics2D.Raycast(transform.position, Vector2.down, _info.JumpRaycastHeight, _jumpRaycastLayer);
-                if (hit.collider != null)
+                var maxSpeed = _info.Speed * Time.deltaTime;
+                var curr = _info.Speed * Time.deltaTime * XMov * .01f + _rb.velocity.x;
+                if (Mathf.Abs(curr) < maxSpeed)
                 {
-                    _rb.AddForce(Vector2.up * _info.JumpForce, ForceMode2D.Impulse);
+                    _rb.velocity = new Vector2(curr, _rb.velocity.y);
                 }
             }
         }
 
-        protected void Attack()
-        {
-            if (_canMove)
-            {
-                var go = Instantiate(_info.BulletPrefab, transform.position + Vector3.right * ForwardDirection, Quaternion.identity);
-                go.GetComponent<Rigidbody2D>().AddForce(Vector2.right * _info.BulletForce * ForwardDirection, ForceMode2D.Impulse);
-                Destroy(go, 10f);
-            }
-        }
-
+        /// <summary>
+        /// Called after dash, reenable player controls after some time and then reload dash
+        /// </summary>
         private IEnumerator WaitAndReloadDash()
         {
             _canMove = false;
@@ -85,6 +75,51 @@ namespace MagicalGirlJam.Character
             gameObject.layer = _baseLayer;
             yield return new WaitForSeconds(_info.DashReloadTime);
             _canUseDash = true;
+        }
+
+        private bool IsOnFloor
+        {
+            get
+            {
+                var hit = Physics2D.Raycast(transform.position, Vector2.down, _info.JumpRaycastHeight, _jumpRaycastLayer);
+                return hit.collider != null;
+            }
+        }
+
+        /// <summary>
+        /// Dash allow for player to gain more speed in a direction and dodge attacks but prevent player from moving
+        /// </summary>
+        protected void Dash()
+        {
+            if (_canMove && _canUseDash)
+            {
+                _rb.AddForce(Vector2.right * _info.DashForce * ForwardDirection, ForceMode2D.Impulse);
+                StartCoroutine(WaitAndReloadDash());
+            }
+        }
+
+        /// <summary>
+        /// Jump, launch player up
+        /// </summary>
+        protected void Jump()
+        {
+            if (_canMove && IsOnFloor)
+            {
+                _rb.AddForce(Vector2.up * _info.JumpForce, ForceMode2D.Impulse);
+            }
+        }
+
+        /// <summary>
+        /// Attack, for now throw a sphere in front of the player
+        /// </summary>
+        protected void Attack()
+        {
+            if (_canMove)
+            {
+                var go = Instantiate(_info.BulletPrefab, transform.position + Vector3.right * ForwardDirection, Quaternion.identity);
+                go.GetComponent<Rigidbody2D>().AddForce(Vector2.right * _info.BulletForce * ForwardDirection, ForceMode2D.Impulse);
+                Destroy(go, 10f);
+            }
         }
     }
 }
